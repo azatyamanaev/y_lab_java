ebcc9b8b195ba57ec918f27f77b0a5e730ed4194
azatyamanaev <iamanaev2001@gmail.com>
2024-10-12 17:10:56 +0300

    remove static block from App.java

--- src/main/java/ru/ylab/App.java
+++ src/main/java/ru/ylab/App.java
@@ -88,33 +88,4 @@ public class App {
     public static void shutdown() {
         running = false;
     }
-
-    static {
-        Map<Long, User> users = UserRepository.getUsers();
-
-        User user = new User(IdUtil.generateUserId(), "admin", "admin@mail.ru", "admin", User.Role.ADMIN);
-        users.put(user.getId(), user);
-        user = new User(IdUtil.generateUserId(), "user1", "a@mail.ru", "pass", User.Role.USER);
-        users.put(user.getId(), user);
-        user = new User(IdUtil.generateUserId(), "user2", "b@mail.ru", "pass", User.Role.USER);
-        users.put(user.getId(), user);
-
-        Map<Long, Habit> habits = HabitRepository.getHabits();
-
-        Habit habit = new Habit(IdUtil.generateHabitId(), "h1", "desc1", Habit.Frequency.DAILY, 2L);
-        habits.put(habit.getId(), habit);
-        habit = new Habit(IdUtil.generateHabitId(), "h2", "desc2", Habit.Frequency.DAILY, 2L);
-        habits.put(habit.getId(), habit);
-        habit = new Habit(IdUtil.generateHabitId(), "h3", "desc3", Habit.Frequency.WEEKLY, 2L);
-        habits.put(habit.getId(), habit);
-
-        habit = new Habit(IdUtil.generateHabitId(), "hb1", "desc1", Habit.Frequency.DAILY, 3L);
-        habits.put(habit.getId(), habit);
-        habit = new Habit(IdUtil.generateHabitId(), "hb2", "desc2", Habit.Frequency.WEEKLY, 3L);
-        habits.put(habit.getId(), habit);
-        habit = new Habit(IdUtil.generateHabitId(), "hb3", "desc3", Habit.Frequency.DAILY, 3L);
-        habits.put(habit.getId(), habit);
-        habit = new Habit(IdUtil.generateHabitId(), "hb4", "desc4", Habit.Frequency.WEEKLY, 3L);
-        habits.put(habit.getId(), habit);
-    }
 }
