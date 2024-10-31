d1df37c112861b880ce9eced23c9e0b25168427d
azatyamanaev <iamanaev2001@gmail.com>
2024-10-31 19:52:17 +0300

    make yml config parsing less verbose

--- src/main/java/ru/ylab/config/AppConfig.java
+++ src/main/java/ru/ylab/config/AppConfig.java
@@ -1,26 +1,21 @@
 package ru.ylab.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-
 import liquibase.Liquibase;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.yaml.snakeyaml.Yaml;
 import ru.ylab.config.datasource.LiquibaseConfig;
-import ru.ylab.exception.HttpException;
 import ru.ylab.services.datasource.CPDataSource;
 import ru.ylab.settings.DbSettings;
 import ru.ylab.settings.LiquibaseSettings;
-import ru.ylab.utils.constants.ErrorConstants;
 
-@Slf4j
 @Configuration
 @EnableAspectJAutoProxy
+@PropertySource(value = "classpath:application.yml", factory = YmlPropertySourceFactory.class)
 @ComponentScan(basePackages = "ru.ylab")
 public class AppConfig {
 
@@ -30,48 +25,28 @@ public class AppConfig {
     }
 
     @Bean
-    public DbSettings dbSettings(Yaml yaml) {
-        try (InputStream stream = this.getClass().getClassLoader().getResourceAsStream("application.yml");) {
-            Map<String, Object> map = yaml.load(stream);
+    public DbSettings dbSettings(Yaml yaml, Environment env) {
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String dbName = System.getenv("DB_NAME");
 
-            Map<String, String> datasource = (Map<String, String>) map.get("datasource");
+        String url = env.getProperty("datasource.url");
+        url = url.replace("{DB_HOST}", host);
+        url = url.replace("{DB_PORT}", port);
+        url = url.replace("{DB_NAME}", dbName);
 
-            String host = System.getenv("DB_HOST");
-            String port = System.getenv("DB_PORT");
-            String dbName = System.getenv("DB_NAME");
-
-            String url = datasource.get("url");
-            url = url.replace("{DB_HOST}", host);
-            url = url.replace("{DB_PORT}", port);
-            url = url.replace("{DB_NAME}", dbName);
-
-            DbSettings settings = new DbSettings();
-            settings.setUrl(url);
-            settings.setUsername(datasource.get("username"));
-            settings.setPassword(datasource.get("password"));
-
-            return settings;
-        } catch (IOException e) {
-            throw HttpException.serverError(e.getMessage(), e.getCause())
-                               .addDetail(ErrorConstants.PARSE_ERROR, "application.yml");
-        }
+        return new DbSettings(
+                url,
+                env.getProperty("datasource.username"),
+                env.getProperty("datasource.password"));
     }
 
     @Bean
-    public LiquibaseSettings liquibaseSettings(Yaml yaml) {
-        try (InputStream stream = this.getClass().getClassLoader().getResourceAsStream("application.yml");) {
-            Map<String, Object> map = yaml.load(stream);
-            Map<String, String> liquibase = (Map<String, String>) map.get("liquibase");
-
-            LiquibaseSettings settings = new LiquibaseSettings(
-                    liquibase.get("changelogPath"),
-                    liquibase.get("changelogSchema"),
-                    liquibase.get("defaultSchema"));
-            return settings;
-        } catch (IOException e) {
-            throw HttpException.serverError(e.getMessage(), e.getCause())
-                               .addDetail(ErrorConstants.PARSE_ERROR, "application.yml");
-        }
+    public LiquibaseSettings liquibaseSettings(Environment env) {
+        return new LiquibaseSettings(
+                env.getProperty("liquibase.changelog.path"),
+                env.getProperty("liquibase.changelog.schema"),
+                env.getProperty("liquibase.default.schema"));
     }
 
     @Bean
---
+++ src/main/java/ru/ylab/config/YmlPropertySourceFactory.java
@@ -0,0 +1,28 @@
+package ru.ylab.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+/**
+ * Class for parsing .yml config file.
+ *
+ * @author azatyamanaev
+ */
+public class YmlPropertySourceFactory implements PropertySourceFactory {
+
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource.getResource());
+
+        Properties properties = factory.getObject();
+
+        return new PropertiesPropertySource(resource.getResource().getFilename(), properties);
+    }
+}
