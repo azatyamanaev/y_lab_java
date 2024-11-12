82f4c0eaef7ea8d9e43d0bbceab4d2dc79d5af9f
azatyamanaev <iamanaev2001@gmail.com>
2024-11-12 08:29:47 +0300

    add audit-starter code

---
+++ src/main/java/ru/ylab/auditstarter/config/AuditConfiguration.java
@@ -0,0 +1,18 @@
+package ru.ylab.auditstarter.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.ylab.auditstarter.aspects.AuditAspect;
+
+@Configuration
+@ConditionalOnClass(AuditAspect.class)
+public class AuditConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AuditAspect auditAspect() {
+        return new AuditAspect();
+    }
+}
--- src/test/resources/db/delete_test_data.sql
+++
@@ -1,3 +0,0 @@
-delete from entity.habit_history;
-delete from entity.habits;
-delete from entity.users;
